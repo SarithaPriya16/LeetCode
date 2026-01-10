8b794018fd81a67d15f4cb2e494019d5d9a8ebd1
Javvaji Saritha Priya <23BQ1A1244@vvit.net>
2026-01-10 23:03:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        stack=[]
+        for a in asteroids:
+            alive=True
+            while stack and alive and stack[-1]>0 and a<0:
+                if abs(stack[-1])<abs(a):
+                    stack.pop()
+                elif abs(stack[-1]) == abs(a):
+                    stack.pop()
+                    alive=False
+                else:
+                    alive=False
+        if alive:
+            stack.append(a)
+        return stack
+
+        
